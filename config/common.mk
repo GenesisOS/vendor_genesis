94ccaab984415e961cde24553be1d5d9e8032ab9
Jake Weinstein <jake@aospa.co>
2019-09-02 06:12:35 +0200

    config: Skip boot jars check
    Matches CAF and fixes errors like these
    
    Error: out/target/common/obj/JAVA_LIBRARIES/telephony-common_intermediates/classes.jar contains class file org/codeaurora/ims/ImsMultiIdentityControllerBase$MultiIdentityBinder.class, which is not in the whitelist
    Error: out/target/common/obj/JAVA_LIBRARIES/tcmiface_intermediates/classes.jar contains class file com/quicinc/tcmiface/DpmTcmIface.class, which is not in the whitelist
    Error: out/target/common/obj/JAVA_LIBRARIES/telephony-ext_intermediates/classes.jar contains class file org/codeaurora/internal/BearerAllocationStatus$1.class, which is not in the whitelist
    
    Change-Id: I94f376d14134c80c14188b07eebca01933b7e47a

--- config/common.mk
+++ config/common.mk
@@ -153,6 +153,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     rsync
 
+# Skip boot JAR checks.
+SKIP_BOOT_JARS_CHECK := true
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
