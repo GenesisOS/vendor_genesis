201dddd6b660e49d0cbf3300c8ef2b3183765009
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-02-20 10:47:19 +0530

    vendor: Add feature xml for call recording
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- config/common.mk
+++ config/common.mk
@@ -178,6 +178,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/genesis/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
