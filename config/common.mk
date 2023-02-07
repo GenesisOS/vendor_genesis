eb8516c0e27a74f873dcff32976be9c9633d6558
SamarV-121 <samarvispute121@pm.me>
2023-02-07 12:11:15 +0100

    common: Disable continuous transaction tracing on userdebug builds
    Change-Id: I95657a00e38f1229675682e784b20cca12c119b0

--- config/common.mk
+++ config/common.mk
@@ -235,6 +235,11 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
 
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    debug.sf.enable_transaction_tracing=false
+endif
+
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/genesis/overlay/common
 
