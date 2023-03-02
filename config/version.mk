4d4c4f67784c6afdab6abe191744e074a03ccdcd
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    vendor: Conditionally enable dev keys signing

--- config/version.mk
+++ config/version.mk
@@ -24,3 +24,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.genesis.maintainer=$(GENESIS_MAINTAINER) \
     ro.genesis.releasetype=$(GENESIS_BUILDTYPE) \
     ro.modversion=$(GENESIS_VERSION)
+
+# Signing
+ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard .android-certs/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard .android-certs/verity.pk8))
+PRODUCT_VERITY_SIGNING_KEY := .android-certs/verity
+endif
+ifneq (,$(wildcard .android-certs/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+endif
+endif
