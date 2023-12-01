a4ceca2a7e507ceb400ecc4cd2d3c162e6481154
someone5678 <someone5678@users.noreply.github.com>
2023-12-01 11:56:20 +0900

    vendor: Inherit certification repo if exists
    Change-Id: I238671bc9957f4321c55f054da8b9ee624fc17db

--- config/common.mk
+++ config/common.mk
@@ -217,6 +217,9 @@ CUSTOM_LOCALES += \
     cy_GB \
     fur_IT
 
+# Certification
+$(call inherit-product-if-exists, vendor/certification/config.mk)
+
 # Fonts
 include vendor/genesis/config/fonts.mk
 
