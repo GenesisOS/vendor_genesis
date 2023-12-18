56e9e36f0e3644f086bd27237f2c047b093500ad
Omkar Chandorkar <gotenksIN@aospa.co>
2023-12-18 15:43:30 +0530

    fonts: Update google-sans from husky UD1A.230803.041
    Change-Id: I24aa2a6c510fe51e6525cadceb24faab398342b5
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- config/fonts.mk
+++ config/fonts.mk
@@ -3,4 +3,5 @@ PRODUCT_COPY_FILES += \
     vendor/genesis/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/genesis/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/genesis/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/genesis/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
     vendor/genesis/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
