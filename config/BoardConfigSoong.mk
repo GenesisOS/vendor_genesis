4a552855ce3283f3715133889be15f0abc5be89d
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-22 17:59:03 +0100

    Zap aapt_version_code_defaults
    It was caused by package-cache in case Build.VERSION.INCREMENTAL
    doesn't change across builds, which is always the case for local
    builds, exporting BUILD_NUMBER to something != eng.$USER will fix
    it the next build.
    
    Change-Id: Ic602197e6711ced511a0b102b1140c5db4ec8397

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -28,7 +28,6 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += genesisGlobalVars
 SOONG_CONFIG_genesisGlobalVars += \
-    aapt_version_code \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_needs_client_info_lib \
@@ -111,7 +110,6 @@ TARGET_TRUST_USB_CONTROL_ENABLE ?= 1
 TARGET_TRUST_USB_CONTROL_DISABLE ?= 0
 
 # Soong value variables
-SOONG_CONFIG_genesisGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_genesisGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_genesisGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_genesisGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
