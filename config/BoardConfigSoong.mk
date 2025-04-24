5c4ae0c1da91e0fbc9459dfe21857c0bb6222662
Michael Bestas <mkbestas@lineageos.org>
2025-04-25 00:17:18 +0300

    config: Move additional gralloc 10 usage bits to select()
    Change-Id: If1542af472e74cd953d9ca093dd225dbba028aea

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -28,7 +28,6 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += genesisGlobalVars
 SOONG_CONFIG_genesisGlobalVars += \
-    additional_gralloc_10_usage_bits \
     camera_needs_client_info_lib \
     camera_needs_client_info_lib_oplus \
     target_camera_package_name \
@@ -39,17 +38,20 @@ SOONG_CONFIG_genesisGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_N
 SOONG_CONFIG_genesisGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 
 # Set default values
-TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_SERVICE_EXT_LIB ?= libcameraservice_ext_lib
 
 # Soong value variables
-SOONG_CONFIG_genesisGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_genesisGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_genesisGlobalVars_target_libcameraservice_ext_lib := $(TARGET_CAMERA_SERVICE_EXT_LIB)
 
 # Camera
 ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(call soong_config_set,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
+endif
+
+# Libui
+ifneq ($(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS),)
+    $(call soong_config_set,libui,additional_gralloc_10_usage_bits,$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 endif
 
 # Lineage Health HAL
